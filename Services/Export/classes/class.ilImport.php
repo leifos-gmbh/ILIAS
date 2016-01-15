fed242c4a4ece67cd031d07b3f65a458819a6401
Jörg Lützenkirchen <luetzenkirchen@leifos.com>
2016-01-15 12:58:51 +0100

    plugin: standalone import/export
    Conflicts:
    	Customizing/global/plugins/Services/Repository/RepositoryObject/Example/classes/class.ilExampleImporter.php

--- Services/Export/classes/class.ilImport.php
+++ Services/Export/classes/class.ilImport.php
@@ -158,12 +158,11 @@ class ilImport
 	 * @param	string		absolute filename of temporary upload file
 	 */
 	protected function doImportObject($dir, $a_type, $a_component = "", $a_tmpdir = "")
-	{
-		global $objDefinition;
-
+	{		
 		if ($a_component == "")
 		{
-			$a_component = $objDefinition->getComponentForType($a_type);		
+			include_once("./Services/Export/classes/class.ilImportExportFactory.php");
+			$a_component = ilImportExportFactory::getComponentForExport($a_type);		
 		}		
 		$this->comp = $a_component;
 
@@ -220,7 +219,7 @@ class ilImport
 		// we should only get on mapping here
 		$top_mapping = $this->mapping->getMappingsOfEntity($this->comp, $a_type);
 		$new_id = (int) current($top_mapping);
-
+		
 		return $new_id;
 	}
 

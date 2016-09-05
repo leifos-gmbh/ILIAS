ae48239d0dc27ac993ba4605a0e4e791ae68c34c
smeyer-ilias <smeyer.ilias@gmx.de>
2016-09-05 17:08:42 +0200

    Added public section domain filter
    Conflicts:
    	Services/Init/classes/class.ilInitialisation.php
    	Services/Init/classes/class.ilStartUpGUI.php

--- Services/Init/classes/class.ilStartUpGUI.php
+++ Services/Init/classes/class.ilStartUpGUI.php
@@ -1058,7 +1058,8 @@ class ilStartUpGUI
 			$rtpl->parseCurrentBlock();
 		}
 
-		if ($ilSetting->get("pub_section") &&
+		include_once './Services/Init/classes/class.ilPublicSectionSettings.php';		
+		if (ilPublicSectionSettings::getInstance()->isEnabledForDomain($_SERVER['SERVER_NAME']) &&
 			$ilAccess->checkAccessOfUser(ANONYMOUS_USER_ID, "read", "", ROOT_FOLDER_ID))
 		{
 			$rtpl->setCurrentBlock("homelink");
@@ -1505,7 +1506,8 @@ class ilStartUpGUI
 		//instantiate logout template
 		self::initStartUpTemplate("tpl.logout.html");
 		
-		if ($ilSetting->get("pub_section"))
+		include_once './Services/Init/classes/class.ilPublicSectionSettings.php';		
+		if (ilPublicSectionSettings::getInstance()->isEnabledForDomain($_SERVER['SERVER_NAME']))
 		{
 			$tpl->setCurrentBlock("homelink");
 			$tpl->setVariable("CLIENT_ID","?client_id=".$client_id."&lang=".$lng->getLangKey());
@@ -1817,11 +1819,7 @@ class ilStartUpGUI
 			ilInitialisation::redirectToStartingPage();
 			return;
 		}
-		else
-		{
-			
-		}
-
+		
 		// no valid session => show client list, if no client info is given
 		if (
 			!isset($_GET["client_id"]) &&
@@ -1831,14 +1829,16 @@ class ilStartUpGUI
 			return $this->showClientList();
 		}
 
-		if($GLOBALS['ilSetting']->get('pub_section', false)
-		)
+		include_once './Services/Init/classes/class.ilPublicSectionSettings.php';		
+		if (ilPublicSectionSettings::getInstance()->isEnabledForDomain($_SERVER['SERVER_NAME']))
 		{
 			return ilInitialisation::goToPublicSection();
 		}
-		
+
 		// otherwise show login page
 		return $this->showLoginPage();
+		
+		
 	}
 
 
@@ -2200,7 +2200,8 @@ class ilStartUpGUI
 			$tpl->setVariable('LINK_URL', 'login.php?cmd=force_login&'.$param);
 			$tpl->parseCurrentBlock();
 
-			if($ilSetting->get('pub_section') &&
+			include_once './Services/Init/classes/class.ilPublicSectionSettings.php';
+			if(ilPublicSectionSettings::getInstance()->isEnabledForDomain($_SERVER['SERVER_NAME']) &&
 				$ilAccess->checkAccessOfUser(ANONYMOUS_USER_ID, 'read', '', ROOT_FOLDER_ID))
 			{
 				$tpl->setVariable('LINK_URL', 'index.php?'.$param);

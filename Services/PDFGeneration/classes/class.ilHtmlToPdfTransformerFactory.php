a1b59549279a126971ad993c924d42558672aa52
Guido Vollbach <gvollbach@databay.de>
2017-01-26 13:04:01 +0100

    missing include

--- Services/PDFGeneration/classes/class.ilHtmlToPdfTransformerFactory.php
+++ Services/PDFGeneration/classes/class.ilHtmlToPdfTransformerFactory.php
@@ -1,6 +1,6 @@
 <?php
 /* Copyright (c) 1998-2015 ILIAS open source, Extended GPL, see docs/LICENSE */
-
+require_once __DIR__ . '/class.ilAbstractHtmlToPdfTransformer.php';
 /**
  * Class ilHtmlToPdfTransformerFactory
  * @author Michael Jansen <mjansen@databay.de>

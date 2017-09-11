1a50aa1f452101006918cc17b893829007eb0b63
mjansen <mjansen@databay.de>
2017-09-11 13:02:44 +0200

    Fixed PHP 5.2.x issues

--- scripts/archive.php
+++ scripts/archive.php
@@ -7,105 +7,122 @@ while(!file_exists($ilias_main_directory . 'ilias.ini.php'))
 }
 chdir($ilias_main_directory);
 
-include_once 'Services/Context/classes/class.ilContext.php';
-ilContext::init(ilContext::CONTEXT_CRON);
-
-include_once 'Services/Authentication/classes/class.ilAuthFactory.php';
-ilAuthFactory::setContext(ilAuthFactory::CONTEXT_CRON);
-
-$_COOKIE['ilClientId'] = $_SERVER['argv'][3];
-$_POST['username'] = $_SERVER['argv'][1];
-$_POST['password'] = $_SERVER['argv'][2];
-$cat_ref_id = $_SERVER['argv'][4];
+include_once './Services/Cron/classes/class.ilCronStartUp.php';
 
 if($_SERVER['argc'] < 5)
 {
-	die('Usage: '. basename(__FILE__) .' username password client ref_id');
+	echo('Usage: '. basename(__FILE__) .' username password client ref_id');
+	exit(1);
 }
 
-require_once 'include/inc.header.php';
+$cat_ref_id = $_SERVER['argv'][4];
 
-/**
- * @var $ilDB ilDB
- * @var $tree ilTree
- * @var $ilObjDataCache ilObjectDataCache
- */
-global $ilDB, $tree, $ilObjDataCache;
+$cron = new ilCronStartUp($_SERVER['argv'][3], $_SERVER['argv'][1], $_SERVER['argv'][2]);
 
-$cat_ref_id = trim($cat_ref_id);
-if(!$cat_ref_id)
-{
-	exit();
-}
+$executionChain = function() use ($cat_ref_id) {
+	/**
+	 * @var $ilDB ilDB
+	 * @var $tree ilTree
+	 * @var $ilObjDataCache ilObjectDataCache
+	 */
+	global $ilDB, $tree, $ilObjDataCache;
 
-$obj_id_cat = $ilObjDataCache->lookupObjId($cat_ref_id);
-if(!$obj_id_cat)
-{
-	echo 'obj_id for ref_id '.$cat_ref_id.' could not be determined. Script execution aborted!' . "\n";
-	exit();
-}
-else
-{
-	echo 'Determined obj_id ' . $obj_id_cat . ' for ref_id '.$cat_ref_id  . "\n" ;
-}
+	$cat_ref_id = trim($cat_ref_id);
+	if(!$cat_ref_id)
+	{
+		exit();
+	}
 
-$title = $ilObjDataCache->lookupTitle($obj_id_cat);
-if('cat' != $ilObjDataCache->lookupType($obj_id_cat))
-{
-	echo 'Object (title:'.$title.'|ref_id:'.$cat_ref_id.') is not of type "cat". Script execution aborted!' . "\n";
-	exit();
-}
+	$obj_id_cat = $ilObjDataCache->lookupObjId($cat_ref_id);
+	if(!$obj_id_cat)
+	{
+		echo 'obj_id for ref_id '.$cat_ref_id.' could not be determined. Script execution aborted!' . "\n";
+		exit();
+	}
+	else
+	{
+		echo 'Determined obj_id ' . $obj_id_cat . ' for ref_id '.$cat_ref_id  . "\n" ;
+	}
 
-if($tree->isDeleted($cat_ref_id))
-{
-	echo 'Category (title:'.$title.'|ref_id:'.$cat_ref_id.') is in trash. Script execution aborted!' . "\n";
-	exit();
-}
+	$title = $ilObjDataCache->lookupTitle($obj_id_cat);
+	if('cat' != $ilObjDataCache->lookupType($obj_id_cat))
+	{
+		echo 'Object (title:'.$title.'|ref_id:'.$cat_ref_id.') is not of type "cat". Script execution aborted!' . "\n";
+		exit();
+	}
 
-$node = $tree->getNodeData($cat_ref_id);
+	if($tree->isDeleted($cat_ref_id))
+	{
+		echo 'Category (title:'.$title.'|ref_id:'.$cat_ref_id.') is in trash. Script execution aborted!' . "\n";
+		exit();
+	}
 
-if(!is_object($GLOBALS['ilSetting']) or $GLOBALS['ilSetting']->getModule() != 'common')
-{
-	include_once './Services/Administration/classes/class.ilSetting.php';
-	$setting = new ilSetting('common');
-}
-else
-{
-	$setting = $GLOBALS['ilSetting'];
-}
+	$node = $tree->getNodeData($cat_ref_id);
 
-$is_ns = $setting->get('main_tree_impl','ns') == 'ns';
-if($is_ns)
-{
-	$query = '
+	if(!is_object($GLOBALS['ilSetting']) or $GLOBALS['ilSetting']->getModule() != 'common')
+	{
+		include_once './Services/Administration/classes/class.ilSetting.php';
+		$setting = new ilSetting('common');
+	}
+	else
+	{
+		$setting = $GLOBALS['ilSetting'];
+	}
+
+	$is_ns = $setting->get('main_tree_impl','ns') == 'ns';
+	if($is_ns)
+	{
+		$query = '
         SELECT od.obj_id
         FROM object_data od
         INNER JOIN object_reference objr ON objr.obj_id = od.obj_id AND objr.deleted IS NULL
         INNER JOIN tree t ON t.child = objr.ref_id AND t.tree = %s AND (t.lft BETWEEN %s AND %s)
         WHERE od.type = %s
     ';
-}
-else
-{
-	$query = '
+	}
+	else
+	{
+		$query = '
         SELECT od.obj_id
         FROM object_data od
         INNER JOIN object_reference objr ON objr.obj_id = od.obj_id AND objr.deleted IS NULL
         INNER JOIN tree t ON t.child = objr.ref_id AND t.tree = %s AND (t.path BETWEEN %s AND %s)
         WHERE od.type = %s
     ';
-}
-$query .= ' AND ' . str_replace('LIKE', 'NOT LIKE', $ilDB->like('od.title', 'text', '[SEMESTER]%%'));
+	}
+	$query .= ' AND ' . str_replace('LIKE', 'NOT LIKE', $ilDB->like('od.title', 'text', '[SEMESTER]%%'));
 
-$manipulation = "
+	$manipulation = "
     UPDATE object_data od
     INNER JOIN ($query) odinner ON odinner.obj_id = od.obj_id
     SET od.title = " . $ilDB->concat(array(array("'[SEMESTER] '", 'text'), array('od.title', '')));
 
-$affected_rows = $ilDB->manipulateF(
-	$manipulation,
-	array('integer', $is_ns ? 'integer' : 'text', $is_ns ? 'integer' : 'text', 'text'),
-	array(1, $is_ns ? $node['lft'] : $node['path'], $is_ns ? $node['rgt'] : $node['path'] . 'Z', 'crs')
-);
+	$affected_rows = $ilDB->manipulateF(
+		$manipulation,
+		array('integer', $is_ns ? 'integer' : 'text', $is_ns ? 'integer' : 'text', 'text'),
+		array(1, $is_ns ? $node['lft'] : $node['path'], $is_ns ? $node['rgt'] : $node['path'] . 'Z', 'crs')
+	);
 
-echo ((int)$affected_rows) . ' course title(s) changed' . "\n";
+	echo ((int)$affected_rows) . ' course title(s) changed' . "\n";
+};
+
+try {
+	$cron->initIlias();
+	$cron->authenticate();
+
+	if(!defined("ILIAS_HTTP_PATH"))
+	{
+		define("ILIAS_HTTP_PATH", ilUtil::_getHttpPath());
+	}
+
+	$executionChain();
+
+	$cron->logout();
+}
+catch(Exception $e)
+{
+	$cron->logout();
+
+	echo $e->getMessage()."\n";
+	exit(1);
+}

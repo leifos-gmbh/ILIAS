bfc68e138aa015127482d229c706290290fa60b8
Alexander Killing <killing@leifos.de>
2018-03-02 17:02:50 +0100

    fixed bug #42: Auto Mail Versand mit Umfrageergebnissen funktioniert immer noch nicht

--- Modules/Survey/classes/class.ilObjSurvey.php
+++ Modules/Survey/classes/class.ilObjSurvey.php
@@ -6393,12 +6393,10 @@ class ilObjSurvey extends ilObject
 				"", "", $subject, $message, array($att), array("system"));
 		}
 		
-		/*
 		$ilDB->manipulate("UPDATE svy_vy".
 			" SET tutor_res_cron = ".$ilDB->quote(1, "integer").
 			" WHERE survey_id = ".$ilDB->quote($this->getSurveyId(), "integer"));		 
-		*/
-		
+
 		return true;
 	}
 	

3d143c40d6d4cd560a92ed5834c717f5ca361e5b
Alexander Killing <killing@leifos.de>
2018-07-20 15:01:10 +0200

    added bulk upload

--- setup/sql/dbupdate_custom.php
+++ setup/sql/dbupdate_custom.php
@@ -2,3 +2,15 @@
 <?php
 	$ilCtrlStructureReader->getStructure();
 ?>
+<#2>
+<?php
+
+if (!$ilDB->tableColumnExists('media_item', 'upload_hash'))
+{
+	$ilDB->addTableColumn('media_item', 'upload_hash', array(
+		"type" => "text",
+		"length" => 100
+	));
+}
+
+?>

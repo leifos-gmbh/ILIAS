662a69a1f991011176981293cf75dbd88d57460f
smeyer-ilias <smeyer.ilias@gmx.de>
2018-09-18 13:57:54 +0200

    new auth plugin init

--- sso/mssso.php
+++ sso/mssso.php
@@ -1,9 +1,26 @@
+
 <?php
+
+chdir ('..');
 
 $attributes = base64_encode($_SERVER['HTTP_X_TRUSTED_REMOTE_ATTR']);
 $user = base64_encode($_SERVER['HTTP_X_TRUSTED_REMOTE_USER']);
 
-header('Location: http://www.unirep-online.de/login.php?force_auth=1&mssso_attrbs='.$attributes.'&mssso_user='.$user);
-exit;
+/**
+ * LTI launch target script
+ *
+ * @author  Stefan Meyer <smeyer.ilias@gmx.de>
+ */
+include_once './Services/Context/classes/class.ilContext.php';
+ilContext::init(ilContext::CONTEXT_APACHE_SSO);
+
+require_once("Services/Init/classes/class.ilInitialisation.php");
+ilInitialisation::initILIAS();
+
+// authentication is done here ->
+$ilCtrl->initBaseClass("ilStartUpGUI");
+$ilCtrl->setCmd('doStandardAuthentication');
+$ilCtrl->setTargetScript("ilias.php");
+$ilCtrl->callBaseClass();
 
 ?>

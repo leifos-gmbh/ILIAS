eb6adcc55c152c3d7984985bc0a372a73a318fef
Alexander Killing <killing@leifos.de>
2018-10-23 16:20:35 +0200

    merged changes from 5_4_ks_filter up to d4865815524cb3e6c3c8a227c8e423fdab79fa51

--- src/UI/templates/js/Input/Container/filter.js
+++ src/UI/templates/js/Input/Container/filter.js
@@ -24,14 +24,41 @@ il.UI = il.UI || {};
 				// no popover yet, we are still in the same input group and search for the il-filter-field span
 				$("#" + id).parents(".input-group").find("span.il-filter-field").html(value_as_string);
 			}
+
+            var input_name = $("#" + id).attr("name");
+            var input_num = input_name.substring(13);
+            var input_label = $("#" + id).parents(".input-group").find("#leftaddon").html();
+            if (input_label == undefined) {
+                var old_input_label = $("#" + input_num).html();
+                var last_char = old_input_label.indexOf(":");
+                old_input_label = old_input_label.substring(0, last_char);
+                $("span[id='" + input_num + "']").html(old_input_label + ":" + value_as_string + ",");
+            } else {
+                $("span[id='" + input_num + "']").html(input_label + ":" + value_as_string + ",");
+			}
 		};
+
+        /**
+         *
+         * @param event
+         * @param id
+         */
+        var onRemoveClick = function(event, id) {
+            // hide the Input in the Filter which should be removed
+        };
 
 		/**
 		 * Public interface
 		 */
 		return {
-			onFieldUpdate: onFieldUpdate
+			onFieldUpdate: onFieldUpdate,
+			onRemoveClick: onRemoveClick
 		};
 
 	})($);
 })($, il.UI);
+
+//Popover of Add-Button always at the bottom
+$(document).ready(function() {
+    $('.input-group .btn.btn-bulky').attr('data-placement', 'bottom');
+});

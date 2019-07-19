9be409ff00f93cf388f0a763f7f53cb07806ad2a
Alexander Killing <killing@leifos.de>
2019-07-19 13:41:40 +0200

    booking resources in courses, added missing db update

---
+++ setup/sql/dbupdate_custom.php
@@ -0,0 +1,60 @@
+<#1>
+<?php
+	if (!$ilDB->tableExists("book_obj_use_book"))
+	{
+		$fields = array(
+			"obj_id" => array(
+				"type" => "integer",
+				"notnull" => true,
+				"length" => 4,
+				"default" => 0
+			),
+			"book_obj_id" => array(
+				"type" => "integer",
+				"notnull" => true,
+				"length" => 4,
+				"default" => 0
+			)
+		);
+	 	$ilDB->createTable("book_obj_use_book", $fields);
+	 }
+?>
+<#2>
+<?php
+	$ilDB->addPrimaryKey("book_obj_use_book", array("obj_id", "book_obj_id"));
+?>
+<#3>
+<?php
+if(!$ilDB->tableColumnExists('booking_reservation','context_obj_id'))
+{
+	$ilDB->addTableColumn(
+		'booking_reservation',
+		'context_obj_id',
+		array(
+			'type' => 'integer',
+			'length' => 1,
+			'notnull' => false,
+			'default' => 0
+		));
+}
+
+?>
+<#4>
+<?php
+$ilDB->dropTableColumn('booking_reservation', 'context_obj_id');
+
+if(!$ilDB->tableColumnExists('booking_reservation','context_obj_id'))
+{
+	$ilDB->addTableColumn(
+		'booking_reservation',
+		'context_obj_id',
+		array(
+			'type' => 'integer',
+			'length' => 4,
+			'notnull' => false,
+			'default' => 0
+		));
+}
+
+?>
+

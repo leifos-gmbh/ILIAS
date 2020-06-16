7a63e019e19dda6d3c386c08ddcb0e8e4f30d46c
Thomas Famula <famula@leifos.de>
2020-06-16 18:02:47 +0200

    Specific Values for Competence Levels

--- setup/sql/5_4_hotfixes.php
+++ setup/sql/5_4_hotfixes.php
@@ -1328,3 +1328,21 @@ if (!$idx) {
     $setting->set('ilfrmreadidx1', 1);
 }
 ?>
+<#90>
+<?php
+if (!$ilDB->tableColumnExists('skl_user_skill_level', 'next_level_fulfilment')) {
+    $ilDB->addTableColumn("skl_user_skill_level", "next_level_fulfilment", array(
+        "type" => "float",
+        "notnull" => true,
+        "default" => 0.0
+    ));
+}
+
+if (!$ilDB->tableColumnExists('skl_user_has_level', 'next_level_fulfilment')) {
+    $ilDB->addTableColumn("skl_user_has_level", "next_level_fulfilment", array(
+        "type" => "float",
+        "notnull" => true,
+        "default" => 0.0
+    ));
+}
+?>

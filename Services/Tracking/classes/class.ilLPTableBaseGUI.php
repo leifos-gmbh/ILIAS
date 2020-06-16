7570e42a9fcdad106e9fb4cefd3c456f06d072f2
Marvin Barz <barz@leifos.de>
2020-06-16 15:54:52 +0200

    Fixed false position of new filter options for usage intensity

--- Services/Tracking/classes/class.ilLPTableBaseGUI.php
+++ Services/Tracking/classes/class.ilLPTableBaseGUI.php
@@ -388,8 +388,6 @@ class ilLPTableBaseGUI extends ilTable2GUI
             $options['lres'] = $lng->txt('obj_lrss');
         }
 
-        $options['root'] = $lng->txt('obj_reps');
-        $options['cat'] = $lng->txt('cat');
         $options['crs'] = $lng->txt('objs_crs');
         $options['grp'] = $lng->txt('objs_grp');
         $options['exc'] = $lng->txt('objs_exc');
@@ -402,6 +400,8 @@ class ilLPTableBaseGUI extends ilTable2GUI
         $options['copa'] = $lng->txt('objs_copa');
         
         if ($a_allow_undefined_lp) {
+            $options['root'] = $lng->txt('obj_reps');
+            $options['cat'] = $lng->txt('objs_cat');
             $options["webr"] = $lng->txt("objs_webr");
             $options["wiki"] = $lng->txt("objs_wiki");
             $options["blog"] = $lng->txt("objs_blog");

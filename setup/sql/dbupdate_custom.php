4a851235f0dcad2579ad536eed70a10cdd10f919
Thomas Famula <famula@leifos.de>
2020-07-08 16:00:35 +0200

    Patch: Manual Sorting for Competences in Competence Profiles - dbupdate

--- setup/sql/dbupdate_custom.php
+++ setup/sql/dbupdate_custom.php
@@ -16,3 +16,15 @@ if (!$ilDB->tableColumnExists('skl_user_has_level', 'next_level_fulfilment')) {
     ));
 }
 ?>
+<#2>
+<?php
+if (!$ilDB->tableColumnExists('skl_profile_level', 'order_nr'))
+{
+    $ilDB->addTableColumn('skl_profile_level', 'order_nr', array(
+        "type" => "integer",
+        "notnull" => true,
+        "default" => 0,
+        "length" => 4
+    ));
+}
+?>

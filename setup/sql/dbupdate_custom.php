89d0506bf4e34bb6902a96e822356182a2f7a240
Thomas Famula <famula@leifos.de>
2020-07-16 14:40:32 +0200

    Manual Sorting for Competences in Competence Profiles - dbupdate fix for existing profiles without order nr

--- setup/sql/dbupdate_custom.php
+++ setup/sql/dbupdate_custom.php
@@ -28,3 +28,28 @@ if (!$ilDB->tableColumnExists('skl_profile_level', 'order_nr'))
     ));
 }
 ?>
+<#3>
+<?php
+if ($ilDB->tableExists('skl_profile_level')) {
+    $profiles = ilSkillProfile::getProfiles();
+    if (!empty($profiles)) {
+        foreach ($profiles as $id => $profile) {
+            $set = $ilDB->query(
+                "SELECT profile_id, base_skill_id, tref_id, order_nr FROM skl_profile_level WHERE " .
+                " profile_id = " . $ilDB->quote($id, "integer")
+            );
+            $cnt = 1;
+            while ($rec = $ilDB->fetchAssoc($set)) {
+                $ilDB->manipulate(
+                    "UPDATE skl_profile_level SET " .
+                    " order_nr = " . $ilDB->quote(($cnt * 10), "integer") .
+                    " WHERE profile_id = " . $ilDB->quote($rec["profile_id"], "integer") .
+                    " AND base_skill_id = " . $ilDB->quote($rec["base_skill_id"], "integer") .
+                    " AND tref_id = " . $ilDB->quote($rec["tref_id"], "integer")
+                );
+                $cnt++;
+            }
+        }
+    }
+}
+?>

09a13829b20cf1ad2d8b24f8f76157913c55dc6f
smeyer <smeyer.ilias@gmx.de>
2020-08-11 11:54:34 +0200

    490: SS - Benutzer anlegen: added missing exception

---
+++ Services/User/exceptions/class.ilUserImportException.php
@@ -0,0 +1,14 @@
+<?php
+
+include_once './Services/User/exceptions/class.ilUserException.php';
+
+/**
+ * Class ilUsersGalleryUsers
+ * @author Stefan Meyer <smeyer.ilias@gmx.de>
+ */
+class ilUserImportException extends ilUserException
+{
+    const ERR_MULTIPLE_IMPORT_IDS = 100;
+}
+
+?>

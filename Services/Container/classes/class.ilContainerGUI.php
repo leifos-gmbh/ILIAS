2ff8dc2169dc720abdba8aeacc11d949d81363f0
Alexander Killing <killing@leifos.de>
2020-09-08 10:46:46 +0200

    permalink for competences screen in courses/groups

--- Services/Container/classes/class.ilContainerGUI.php
+++ Services/Container/classes/class.ilContainerGUI.php
@@ -3850,4 +3850,15 @@ class ilContainerGUI extends ilObjectGUI implements ilDesktopItemHandling
     {
         $this->ctrl->setReturn($this, "");
     }
+
+    /**
+     * Redirect to competences
+     */
+    public function competencesObject()
+    {
+        $ctrl = $this->ctrl;
+
+        $ctrl->redirectByClass(["ilContainerSkillGUI", "ilContSkillPresentationGUI"]);
+    }
+
 }

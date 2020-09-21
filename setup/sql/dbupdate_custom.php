b8437f09ef4f047d2fcc53b7300231e5185bbde3
Thomas Famula <famula@leifos.de>
2020-09-21 12:07:15 +0200

    Local Competence Profiles in Courses and Groups - dbupdate custom

---
+++ setup/sql/dbupdate_custom.php
@@ -0,0 +1,11 @@
+<#1>
+<?php
+if (!$ilDB->tableColumnExists("skl_profile", "ref_id")) {
+    $ilDB->addTableColumn("skl_profile", "ref_id", array(
+        "type" => "integer",
+        "notnull" => true,
+        "default" => 0,
+        "length" => 4
+    ));
+}
+?>

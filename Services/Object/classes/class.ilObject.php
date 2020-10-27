89f9a632d93f921539e1ef33b968469bb3d29957
smeyer <smeyer.ilias@gmx.de>
2020-10-27 12:46:31 +0100

    dtpl icon: presentation

--- Services/Object/classes/class.ilObject.php
+++ Services/Object/classes/class.ilObject.php
@@ -2026,6 +2026,13 @@ class ilObject
                 $filename = $customIcon->getFullPath();
                 return $filename . '?tmp=' . filemtime($filename);
             }
+
+            $dtpl_icon_factory = ilDidacticTemplateIconFactory::getInstance();
+            $path = $dtpl_icon_factory->getIconPathForObject((int) $a_obj_id);
+            if ($path) {
+                return $path . '?tmp=' . filemtime($path);
+            }
+
         }
 
         if (!$a_offline) {

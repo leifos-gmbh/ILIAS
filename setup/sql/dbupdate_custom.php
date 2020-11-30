acfcee57e45ce7cdf9b2592156888d7e0f847b2b
smeyer <smeyer.ilias@gmx.de>
2020-11-30 21:06:34 +0100

    adv int: custom update

--- setup/sql/dbupdate_custom.php
+++ setup/sql/dbupdate_custom.php
@@ -261,5 +261,173 @@ while ($row = $res->fetchRow(ilDBConstants::FETCHMODE_OBJECT)) {
     }
 }
 ?>
+<#11>
+<?php
+
+if (!$ilDB->tableExists('adv_mdf_enum')) {
+    $ilDB->createTable('adv_mdf_enum', [
+        'field_id' => [
+            'type' => ilDBConstants::T_INTEGER,
+            'length' => 4,
+            'notnull' => true,
+        ],
+        'lang_code' => [
+            'type' => ilDBConstants::T_TEXT,
+            'notnull' => true,
+            'length' => 5
+        ],
+        'idx' => [
+            'type' => ilDBConstants::T_INTEGER,
+            'length' => 4,
+            'notnull' => true,
+        ],
+        'value' => [
+            'type' => ilDBConstants::T_TEXT,
+            'length' => 4000,
+            'notnull' => true
+        ]
+    ]);
+    $ilDB->addPrimaryKey('adv_mdf_enum', array('field_id', 'lang_code', 'idx'));
+}
+?>
+<#12>
+<?php
+
+$query = 'select value from settings where  module = ' . $ilDB->quote('common', ilDBConstants::T_TEXT) . ' ' .
+    'and keyword = ' . $ilDB->quote('language', ilDBConstants::T_TEXT);
+$res = $ilDB->query($query);
+$default = 'en';
+while ($row  = $res->fetchRow(ilDBConstants::FETCHMODE_OBJECT)) {
+    $default = $row->value;
+}
+$query = 'update adv_md_record set lang_default = ' . $ilDB->quote($default, ilDBConstants::T_TEXT) . ' ' .
+    'where lang_default IS NULL';
+$ilDB->query($query);
+?>
+
+<#13>
+<?php
+$query = 'select * from adv_md_record ';
+$res = $ilDB->query($query);
+while ($row = $res->fetchRow(ilDBConstants::FETCHMODE_OBJECT)) {
+    $query = 'select * from adv_md_record_int ' .
+        'where record_id = ' . $ilDB->quote($row->record_id, ilDBConstants::T_INTEGER) . ' ' .
+        'and lang_code = ' . $ilDB->quote($row->lang_default, ilDBConstants::T_TEXT);
+    $int_res = $ilDB->query($query);
+    if ($int_res->numRows()) {
+        continue;
+    }
+    $query = 'insert into adv_md_record_int (record_id, title, description, lang_code ) ' .
+    'values ( ' .
+    $ilDB->quote($row->record_id, ilDBConstants::T_INTEGER) . ', ' .
+    $ilDB->quote($row->title, ilDBConstants::T_TEXT) . ', ' .
+    $ilDB->quote($row->description, ilDBConstants::T_TEXT) . ', ' .
+    $ilDB->quote($row->lang_default, ilDBConstants::T_TEXT) .
+    ')' ;
+    $ilDB->manipulate($query);
+}
+?>
+
+<#14>
+<?php
+$query = 'select * from adv_mdf_definition advf ' .
+    'join adv_md_record advr on advf.record_id = advr.record_id ';
+$res = $ilDB->query($query);
+while ($row = $res->fetchRow(ilDBConstants::FETCHMODE_OBJECT)) {
+    $query = 'select * from adv_md_field_int ' .
+        'where field_id = ' . $ilDB->quote($row->field_id, ilDBConstants::T_INTEGER) . ' ' .
+        'and lang_code = ' . $ilDB->quote($row->lang_default, ilDBConstants::T_TEXT);
+    $int_res = $ilDB->query($query);
+    if ($int_res->numRows()) {
+        continue;
+    }
+    $query = 'insert into adv_md_field_int (field_id, title, description, lang_code ) ' .
+        'values ( ' .
+        $ilDB->quote($row->field_id, ilDBConstants::T_INTEGER) . ', ' .
+        $ilDB->quote($row->title, ilDBConstants::T_TEXT) . ', ' .
+        $ilDB->quote($row->description, ilDBConstants::T_TEXT) . ', ' .
+        $ilDB->quote($row->lang_default, ilDBConstants::T_TEXT) .
+        ')' ;
+    $ilDB->manipulate($query);
+}
+?>
+
+<#15>
+<?php
+
+$query = 'select advf.record_id, field_id, field_values, lang_default from adv_mdf_definition advf ' .
+    'join adv_md_record advr on advf.record_id = advr.record_id ' . ' ' .
+    'where ( field_type = ' . $ilDB->quote(1, ilDBConstants::T_INTEGER) . ' or ' .
+    'field_type = ' . $ilDB->quote(8, ilDBConstants::T_INTEGER) . ' ) ';
+
+$res = $ilDB->query($query);
+while ($row = $res->fetchRow(ilDBConstants::FETCHMODE_OBJECT)) {
+
+    $values = unserialize($row->field_values);
+    if (array_key_exists('options', $values)) {
+        $idx = 0;
+        foreach ($values['options'] as $option) {
+            $query = 'insert into adv_mdf_enum (field_id, lang_code, idx, value ) ' .
+                'values ( ' .
+                $ilDB->quote($row->field_id, ilDBConstants::T_INTEGER) . ', ' .
+                $ilDB->quote($row->lang_default, ilDBConstants::T_TEXT) . ', ' .
+                $ilDB->quote($idx++, ilDBConstants::T_INTEGER) . ', ' .
+                $ilDB->quote($option, ilDBConstants::T_TEXT).
+                ' ) ';
+            $ilDB->manipulate($query);
+        }
+    }
+    if (array_key_exists('option_translations', $values)) {
+        foreach ($values['option_translations'] as $lang => $options) {
+            if ($lang == $row->lang_default) {
+                continue;
+            }
+            $idx = 0;
+            foreach ($options as $option) {
+                $query = 'insert into adv_mdf_enum (field_id, lang_code, idx, value ) ' .
+                    'values ( ' .
+                    $ilDB->quote($row->field_id, ilDBConstants::T_INTEGER) . ', ' .
+                    $ilDB->quote($lang, ilDBConstants::T_TEXT) . ', ' .
+                    $ilDB->quote($idx++, ilDBConstants::T_INTEGER) . ', ' .
+                    $ilDB->quote($option, ilDBConstants::T_TEXT).
+                    ' ) ';
+                $ilDB->manipulate($query);
+            }
+        }
+    }
+    if (
+        !array_key_exists('options', $values) &&
+        !array_key_exists('options_translations', $values) &&
+        is_array($values)
+    ) {
+        $idx = 0;
+        foreach ($values as $option) {
+            $query = 'insert into adv_mdf_enum (field_id, lang_code, idx, value ) ' .
+                'values ( ' .
+                $ilDB->quote($row->field_id, ilDBConstants::T_INTEGER) . ', ' .
+                $ilDB->quote($row->lang_default, ilDBConstants::T_TEXT) . ', ' .
+                $ilDB->quote($idx++, ilDBConstants::T_INTEGER) . ', ' .
+                $ilDB->quote($option, ilDBConstants::T_TEXT).
+                ' ) ';
+            $ilDB->manipulate($query);
+        }
+    }
+}
+?>
+<#16>
+<?php
+if (!$ilDB->tableColumnExists('adv_md_values_ltext', 'disabled')) {
+    $ilDB->addTableColumn(
+            'adv_md_values_ltext',
+            'disabled',
+            [
+                'type' => ilDBConstants::T_INTEGER,
+                'notnull' => true,
+                'length' => 1,
+                'default' => 0
+            ]
+        );
+}
+?>
 
 

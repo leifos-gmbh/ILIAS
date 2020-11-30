8c00f5e0c24c3bba183bb60d67d1dcf04db957f3
smeyer <smeyer.ilias@gmx.de>
2020-11-30 16:00:48 +0100

    adv int: db steps

--- setup/sql/dbupdate_custom.php
+++ setup/sql/dbupdate_custom.php
@@ -148,5 +148,118 @@ if (!$ilDB->tableExists('adv_md_values_ltext')) {
 <?php
 $ilCtrlStructureReader->getStructure();
 ?>
+<#9>
+<?php
+if (!$ilDB->tableExists('adv_md_values_enum')) {
+    $ilDB->createTable('adv_md_values_enum', [
+        'obj_id' => [
+            'type' => 'integer',
+            'length' => 4,
+            'notnull' => true,
+            'default' => 0
+        ],
+        'sub_type' => [
+            'type' => 'text',
+            'length' => 10,
+            'notnull' => true,
+            'default' => "-"
+        ],
+        'sub_id' => [
+            'type' => 'integer',
+            'length' => 4,
+            'notnull' => true,
+            'default' => 0
+        ],
+        'field_id' => [
+            'type' => 'integer',
+            'length' => 4,
+            'notnull' => true,
+            'default' => 0
+        ],
+        'disabled' => [
+            'type' => 'integer',
+            'length' => 1,
+            'notnull' => true,
+            'default' => 0
+        ],
+        'value_index' => [
+            'type' => ilDBConstants::T_TEXT,
+            'length' => 16,
+            'notnull' => true,
+        ]
+    ]);
+
+    $ilDB->addPrimaryKey('adv_md_values_enum', array('obj_id', 'sub_type', 'sub_id', 'field_id', 'value_index'));
+}
+?>
+<#10>
+<?php
+
+$query = 'select field_id, field_type, field_values from adv_mdf_definition ' .
+    'where field_type = 1  or field_type = 8 ';
+$res = $ilDB->query($query);
+while ($row = $res->fetchRow(ilDBConstants::FETCHMODE_OBJECT)) {
+    $values = unserialize($row->field_values);
+    if (!is_array($values)) {
+        continue;
+    }
+    $options = $values;
+
+    $query = 'select * from adv_md_values_text ' .
+        'where field_id = ' . $ilDB->quote($row->field_id, ilDBConstants::T_INTEGER);
+    $val_res = $ilDB->query($query);
+    while ($val_row = $val_res->fetchRow(ilDBConstants::FETCHMODE_OBJECT)) {
+
+        $query = 'select * from adv_md_values_enum ' .
+            'where obj_id = ' . $ilDB->quote($val_row->obj_id, ilDBConstants::T_INTEGER) . ' ' .
+            'and sub_id = ' . $ilDB->quote($val_row->sub_id, ilDBConstants::T_INTEGER) . ' ' .
+            'and sub_type = ' . $ilDB->quote($val_row->sub_type, ilDBConstants::T_TEXT) . ' ' .
+            'and field_id = ' . $ilDB->quote($val_row->field_id, ilDBConstants::T_INTEGER);
+        $exists_res = $ilDB->query($query);
+        if ($exists_res->numRows()) {
+            //ilLoggerFactory::getLogger('root')->info('field_id: ' . $val_row->field_id . ' is already migrated');
+            continue;
+        }
+        $current_values = [];
+        if (strpos($val_row->value, '~|~') === 0) {
+            // multi enum
+            $current_values = explode('~|~', $val_row->value);
+            array_pop($current_values);
+            array_shift($current_values);
+
+        } else {
+            $current_values[] = (string) $val_row->value;
+        }
+        //ilLoggerFactory::getLogger('root')->dump($current_values);
+        $positions = [];
+        foreach ($current_values as $value) {
+            if (!strlen(trim($value))) {
+                continue;
+            }
+            $idx = array_search($value, $options);
+            if ($idx === false) {
+                continue;
+            }
+            $positions[] = $idx;
+        }
+
+        //ilLoggerFactory::getLogger('root')->dump($positions);
+        foreach ($positions as $pos) {
+
+            $query = 'insert into adv_md_values_enum (obj_id, sub_type, sub_id, field_id, value_index, disabled) ' .
+            'values ( ' .
+                $ilDB->quote($val_row->obj_id, ilDBConstants::T_INTEGER) . ', '.
+                $ilDB->quote($val_row->sub_type, ilDBConstants::T_TEXT) . ', ' .
+                $ilDB->quote($val_row->sub_id, ilDBConstants::T_INTEGER) . ', ' .
+                $ilDB->quote($val_row->field_id, ilDBConstants::T_INTEGER) . ', ' .
+                $ilDB->quote($pos, ilDBConstants::T_INTEGER) . ', ' .
+                $ilDB->quote($val_row->disabled, ilDBConstants::T_INTEGER)
+                .' ) ';
+            $ilDB->query($query);
+        }
+
+    }
+}
+?>
 
 

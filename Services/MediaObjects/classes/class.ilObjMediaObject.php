e161005f9d8b0f4b4e40ff866ea3fa351f8a27a7
smeyer <smeyer.ilias@gmx.de>
2021-10-28 17:01:32 +0200

    sky: interactive video patch; netscaler

--- Services/MediaObjects/classes/class.ilObjMediaObject.php
+++ Services/MediaObjects/classes/class.ilObjMediaObject.php
@@ -1436,6 +1436,7 @@ class ilObjMediaObject extends ilObject
                 
             // Media Pool
             case "mep":
+            case 'xvid':
                 $obj_id = $id;
                 break;
 

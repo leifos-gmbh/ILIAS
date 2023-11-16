1a231ffd5d91f04e4161a4bbe242e9cc6f09261c
Christoph Ludolf <ludolf@leifos.de>
2023-11-16 11:04:02 +0100

    adds tests for some classes

--- Services/Export/test/ilServicesExportSuite.php
+++ Services/Export/test/ilServicesExportSuite.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
@@ -18,6 +16,8 @@ declare(strict_types=1);
  *
  *********************************************************************/
 
+declare(strict_types=1);
+
 use PHPUnit\Framework\TestSuite;
 
 class ilServicesExportSuite extends TestSuite
@@ -28,6 +28,39 @@ class ilServicesExportSuite extends TestSuite
 
         include_once("./Services/Export/test/ilExportOptionsTest.php");
         $suite->addTestSuite(ilExportOptionsTest::class);
+
+        ilServicesExportSuite::addImportHandlerTests($suite);
+
         return $suite;
+    }
+
+    public static function addImportHandlerTests(ilServicesExportSuite $suite): void
+    {
+        $base_path =
+            "." . DIRECTORY_SEPARATOR
+            . "Services" . DIRECTORY_SEPARATOR
+            . "Export" . DIRECTORY_SEPARATOR
+            . "test" . DIRECTORY_SEPARATOR
+            . "ImportHandler";
+        $dir_infos = [[array_diff(scandir($base_path), array('.', '..')), $base_path]];
+
+        while (count($dir_infos) > 0) {
+            $current_dir_info = array_shift($dir_infos);
+            $dir_files = $current_dir_info[0];
+            $dir_path = $current_dir_info[1];
+            foreach ($dir_files as $dir_file) {
+                $file_path = $dir_path . DIRECTORY_SEPARATOR . $dir_file;
+                if (is_dir($file_path)) {
+                    $new_dir_files = array_diff(scandir($file_path), array('.', '..'));
+                    $dir_infos[] = [$new_dir_files, $file_path];
+                    continue;
+                }
+                if (str_ends_with($dir_file, '.php')) {
+                    include_once($dir_path);
+                    $class_name = substr($dir_file, 0, count($dir_file) - 4);
+                    $suite->addTestSuite($dir_file::class);
+                }
+            }
+        }
     }
 }

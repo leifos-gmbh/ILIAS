87dafa79b472cf436c4ab4e63d0d7331cb44ae15
Tim Schmitz <schmitz@leifos.de>
2025-01-16 12:48:01 +0100

    initial implementation

--- components/ILIAS/WebServices/RPC/classes/class.ilRpcClientFactory.php
+++ components/ILIAS/WebServices/RPC/classes/class.ilRpcClientFactory.php
@@ -1,49 +1,38 @@
 <?php
 
-declare(strict_types=1);
-/*
-    +-----------------------------------------------------------------------------+
-    | ILIAS open source                                                           |
-    +-----------------------------------------------------------------------------+
-    | Copyright (c) 1998-2001 ILIAS open source, University of Cologne            |
-    |                                                                             |
-    | This program is free software; you can redistribute it and/or               |
-    | modify it under the terms of the GNU General Public License                 |
-    | as published by the Free Software Foundation; either version 2              |
-    | of the License, or (at your option) any later version.                      |
-    |                                                                             |
-    | This program is distributed in the hope that it will be useful,             |
-    | but WITHOUT ANY WARRANTY; without even the implied warranty of              |
-    | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
-    | GNU General Public License for more details.                                |
-    |                                                                             |
-    | You should have received a copy of the GNU General Public License           |
-    | along with this program; if not, write to the Free Software                 |
-    | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. |
-    +-----------------------------------------------------------------------------+
-*/
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
 
+declare(strict_types=1);
 
 /**
- * @classDescription Factory for ILIAS rpc client
  * @author Stefan Meyer <meyer@leifos.com>
  */
 class ilRpcClientFactory
 {
     /**
-     * Creates an ilRpcClient instance to our ilServer
-     *
-     * @param string $a_package Package name
-     * @param int $a_timeout The maximum number of seconds to allow ilRpcClient to connect.
-     * @return ilRpcClient
+     * @param string $package Package name
+     * @param int    $timeout The maximum number of seconds to allow ilRpcClient to connect.
      */
-    public static function factory(string $a_package, int $a_timeout = 0): ilRpcClient
+    public static function factory(string $package, int $timeout = 0): ilRpcClient
     {
         return new ilRpcClient(
             ilRPCServerSettings::getInstance()->getServerUrl(),
-            $a_package . '.',
-            $a_timeout,
-            'UTF-8'
+            $package . '.',
+            $timeout
         );
     }
 }

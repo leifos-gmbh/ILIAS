b65bae042065cfb47285d74bd860b32357d644d1
Christoph Ludolf <ludolf@leifos.de>
2025-07-30 15:37:15 +0200

    Poll: ilPollAnswerTableGUI kitchensink implementation, fix table sort bug

--- components/ILIAS/Poll/classes/class.ilPollAnswerTableGUI.php
+++ components/ILIAS/Poll/classes/class.ilPollAnswerTableGUI.php
@@ -27,7 +27,6 @@ use ILIAS\UI\Component\Table\Data as ilDataTable;
 use ILIAS\UI\URLBuilder;
 use ILIAS\UI\URLBuilderToken as ilURLBuilderToken;
 use ILIAS\UI\Renderer as UIRenderer;
-use JetBrains\PhpStorm\NoReturn;
 
 class ilPollAnswerTableGUI
 {

6268ef1d0301f602db18cc824fa717b496558228
Tim Schmitz <schmitz@leifos.de>
2025-11-07 17:57:50 +0100

    initial commit

---
+++ components/ILIAS/Container/Sorting/Positions/PositionData.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
+
+declare(strict_types=1);
+
+namespace ILIAS\Container\Sorting\Positions;
+
+class PositionData
+{
+    public function __construct(
+        protected int $obj_id,
+        protected int $child_obj_id,
+        protected int $position,
+        protected string $parent_type,
+        protected int $parent_id
+    ) {
+    }
+
+    public function getObjId(): int
+    {
+        return $this->obj_id;
+    }
+
+    public function getChildObjId(): int
+    {
+        return $this->child_obj_id;
+    }
+
+    public function getPosition(): int
+    {
+        return $this->position;
+    }
+
+    public function getParentType(): string
+    {
+        return $this->parent_type;
+    }
+
+    public function getParentID(): int
+    {
+        return $this->parent_id;
+    }
+}
---
+++ components/ILIAS/Container/Sorting/Service/RepoService.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
+
+declare(strict_types=1);
+
+namespace ILIAS\Container\Sorting\Service;
+
+use ILIAS\Container\InternalDataService;
+use ilDBInterface;
+use ILIAS\Container\Sorting\Settings\Repository as SettingsRepo;
+
+class RepoService
+{
+    public function __construct(
+        protected InternalDataService $data,
+        protected ilDBInterface $db
+    ) {
+    }
+
+    public function settings(): SettingsRepo
+    {
+        return new SettingsRepo($this->data, $this->db);
+    }
+}
